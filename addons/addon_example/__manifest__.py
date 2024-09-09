fa5293e52872ca844b10f7b1e121e9ba6c1e95c7
Naraka <jcolljeyce@gmail.com>
2024-09-09 15:28:48 +0200

    feat(odoo-sandbox): starting docker compose, addon

---
+++ addons/addon_example/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Mi Módulo',
+    'version': '1.0',
+    'author': 'Mi Nombre',
+    'category': 'Custom',
+    'summary': 'Un módulo de prueba',
+    'depends': ['base'],
+    'data': [
+        'views/mi_modelo_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
